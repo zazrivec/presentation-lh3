12dcd9ea05907d4925d2569c115d99219f912670
Ján Zázrivec <jan.zazrivec@gmail.com>
2013-12-30 22:00:22 +0100

    initial import

---
+++ slide_config.js
@@ -0,0 +1,25 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'LH3 - FE',
+    subtitle: 'Technologické preview',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: false, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: false, // Default: true. If touch support should enabled. Note: the device must support touch.
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ]
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Ján Zázrivec',
+    company: 'SW Architect, Softec',
+    twitter: '@zazrivec',
+    email: 'jan.zazrivec@softec.sk',
+    github: 'http://github.com/zazrivec'
+  }]
+};
+
